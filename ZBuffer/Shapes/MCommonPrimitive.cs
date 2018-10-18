5db34f919f4779057e9f6d45f08c8d5fbf92ff59
Dmitry <waild-cat@mail.ru>
2018-10-19 01:28:17 +0300

    Added CommonPrimitive class

---
+++ ZBuffer/Shapes/MCommonPrimitive.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZBuffer.Shapes
+{
+    public abstract class MCommonPrimitive : MShape
+    {
+        public abstract List<MPoint> GetVertices();
+    }
+}

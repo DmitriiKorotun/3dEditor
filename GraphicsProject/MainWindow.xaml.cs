fcf51c7f3c8db88bdf1e8eb5755d3ef576ed7716
Dmitry <waild-cat@mail.ru>
2018-11-09 14:27:15 +0300

    Reworked rotation algorithm (matrixes instead of quaternions)

--- GraphicsProject/MainWindow.xaml.cs
+++ GraphicsProject/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -37,48 +38,12 @@ namespace GraphicsProject
         {
             var painter = new Painter();
 
-            Random rand = new Random();
-
-            var ls = new Stopwatch();
-            ls.Start();
             var box1 = new MBox(new MPoint(0, 0, 0), 50, 50, 50);
-            var box2 = new MBox(new MPoint(75, 75, 0), 50, 50, 50);
-            var box3 = new MBox(new MPoint(75, 75, 0), 50, 50, 50);
-            var mfacet = new MFacet(new MPoint(50, 35, 0), new MPoint(100, 23, 0), new MPoint(72, 75, 0));
-            var mfacet2 = new MFacet(new MPoint(50, 35, 0), new MPoint(100, 23, 0), new MPoint(72, 75, 0));
-            //new ShapeEditor().RotateY(box1, 20);
-            //new ShapeEditor().RotateZ(box1, 90);
-            //new ShapeEditor().Move(box1, -50, 0, 0);
-            //new ShapeEditor().ProjectShape(mfacet2, Scene.CurrentCamera);
-            //new ShapeEditor().RotateZVertices(box3.GetVertices(), 25);
+
             Scene.AddShape(box1);
-            //Scene.AddShape(mfacet);
-            //Scene.AddShape(mfacet2);
-            //scene.AddShape(box3);
 
-            //for (int i = 0; i < 100; ++i)
-            //{
-            //    scene.AddShape(new MFacet(new MPoint(rand.Next((int)screen.Width), rand.Next((int)screen.Height), 0),
-            //        new MPoint(rand.Next((int)screen.Width), rand.Next((int)screen.Height), 0), new MPoint(rand.Next((int)screen.Width), rand.Next((int)screen.Height), 0)));
-            //}
-            var lol = ls.Elapsed;
-            //scene.AddShape(new MFacet(new MPoint(100, 100, 0), new MPoint(100, 200, 0), new MPoint(200, 100, 0)));
-
-            //painter.DrawPoint(scene, new System.Windows.Media.Media3D.Point3D(100, 100, 100));
-            //painter.DrawFacet(scene, new MFacet(new MPoint(1, 1, 0), new MPoint(100, 100, 0), new MPoint(1, 1, 0)));
-            //Random rand = new Random();
-            //for (int i = 0; i < 10; ++i)
-            //{
-            //    painter.DrawFacet(scene, new MFacet(new MPoint(rand.Next((int)screen.Width), rand.Next((int)screen.Height), 0),
-            //        new MPoint(rand.Next((int)screen.Width), rand.Next((int)screen.Height), 0), new MPoint(1, 1, 0)));
-            //}
-            //painter.DrawFacet(scene, new MFacet(new MPoint(100, 100, 0), new MPoint(80, 80, 0), new MPoint(1, 1, 0)));
-            //painter.DrawFacet(scene, new MFacet(new MPoint(1, 1, 0), new MPoint(100, 100, 0), new MPoint(1, 1, 0)));
-
-            ls.Restart();
             screen.Source = Scene.Render();
-            var lol2 = ls.Elapsed;
-            var lol4 = ls.Elapsed;
+
         }
 
         private void btn_clear_Click(object sender, RoutedEventArgs e)
@@ -101,9 +66,28 @@ namespace GraphicsProject
             MBox mbox = new MBox(leftFaceCorner, width, length, height);
         }
 
-        private void btn_rotateLeft_Click(object sender, RoutedEventArgs e)
+        private async void btn_rotateLeft_Click(object sender, RoutedEventArgs e)
         {
-            Scene.RotateSelected(10);
+            await Task.Run(() =>
+            {
+                var editor = new ShapeEditor();
+
+                for (int i = 0; i < 500; ++i)
+                {
+                    editor.RotateRange(Scene.SelectedShapes, 10, 0, 0);
+
+                    if (screen.Dispatcher.CheckAccess())
+                        Render();
+                    else
+                        screen.Dispatcher.Invoke(new Action(Render));
+
+                    Thread.Sleep(500);
+                }
+            });
+        }
+
+        private void Render()
+        {
             screen.Source = Scene.Render();
         }
 
@@ -153,8 +137,6 @@ namespace GraphicsProject
             if (dialogResult == true)
             {
                 var deserialaziedScene = GraphicsProjectIO.ReadFromXmlFile<Scene>(dlg.FileName);
-
-                deserialaziedScene.Bitmap = Scene.Bitmap;
 
                 Scene = deserialaziedScene;
 
